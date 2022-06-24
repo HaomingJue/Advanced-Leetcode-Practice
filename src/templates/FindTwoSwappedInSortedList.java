fe777789b9ac27bd8967d3051eb3038b83c03f56
haomingjue <haomingjue@outlook.com>
2022-06-23 23:36:09 -0400

    find two swapped elements in sorted list

---
+++ src/templates/FindTwoSwappedInSortedList.java
@@ -0,0 +1,25 @@
+package templates;
+
+import java.util.List;
+
+public class FindTwoSwappedInSortedList {
+    private int[] findTwoSwappedInSortedList(List<Integer> nums) {
+        int n = nums.size();
+        int x = -1, y = -1;
+        boolean swapped_first_occurrence = false;
+        for(int i = 0; i < n - 1; ++i) {
+            if (nums.get(i + 1) < nums.get(i)) {
+                y = nums.get(i + 1);
+                if (!swapped_first_occurrence) {
+                    // first swap occurrence
+                    x = nums.get(i);
+                    swapped_first_occurrence = true;
+                } else {
+                    // second swap occurrence
+                    break;
+                }
+            }
+        }
+        return new int[]{x, y};
+    }
+}
