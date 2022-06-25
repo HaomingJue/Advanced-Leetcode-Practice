bca9e79e6b73d9c669180179d1a3bd115cd1ce2e
haomingjue <haomingjue@outlook.com>
2022-06-25 00:11:14 -0400

    can't understand

---
+++ src/practice/LeetCode99_Recover_Binary_Search_Tree_Morris_Inorder_Traversal.java
@@ -0,0 +1,43 @@
+package practice;
+
+import utils.TreeNode;
+
+public class LeetCode99_Recover_Binary_Search_Tree_Morris_Inorder_Traversal {
+    public void recoverTree(TreeNode root) {
+        TreeNode x = null, y = null, pred = null, predecessor = null;
+        while (root != null) {
+            if (root.left != null) {
+                predecessor = root.left;
+
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                else{
+                    if (pred != null && root.val < pred.val) {
+                        y = root;
+                        if (x == null) x = pred;
+                    }
+                    pred = root;
+
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            else {
+                if (pred != null && root.val < pred.val) {
+                    y = root;
+                    if (x == null) x = pred;
+                }
+                pred = root;
+                root = root.right;
+            }
+        }
+        int tmp = x.val;
+        x.val = y.val;
+        y.val = tmp;
+    }
+}
