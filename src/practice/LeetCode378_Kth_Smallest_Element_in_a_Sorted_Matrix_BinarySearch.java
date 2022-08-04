0022a0b87495b3200c20ae77ce6dbd35918b28c6
haomingjue <haomingjue@outlook.com>
2022-08-03 21:25:31 -0400

    378

---
+++ src/practice/LeetCode378_Kth_Smallest_Element_in_a_Sorted_Matrix_BinarySearch.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class LeetCode378_Kth_Smallest_Element_in_a_Sorted_Matrix_BinarySearch {
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = matrix[0][0];
+        int high = matrix[m - 1][n - 1];
+        while (low < high) {
+            int count = 0;
+            int mid = low + (high - low) / 2;
+            int j = n - 1;
+            for (int i = 0; i < m; i++) {
+                while(j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            if (count < k) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
