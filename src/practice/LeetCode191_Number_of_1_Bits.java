107cfff4455a9c1b123537fcf5324a5480f7548a
haomingjue <haomingjue@outlook.com>
2022-10-07 14:39:07 -0400

    191

---
+++ src/practice/LeetCode191_Number_of_1_Bits.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class LeetCode191_Number_of_1_Bits {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>>= 1;
+        }
+        return count;
+    }
+}
